dfeab18fa388e47d13c1e0f6ef49b05c50f8e378
paul gonzales <paulgonzales@MacBook-Pro.local>
2021-01-18 22:44:21 +0100

    char flags OK

--- srcs/ft_print_char.c
+++ srcs/ft_print_char.c
@@ -6,25 +6,50 @@
 /*   By: gpaul <gpaul@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 15:27:20 by gpaul             #+#    #+#             */
-/*   Updated: 2021/01/18 19:45:38 by gpaul            ###   ########.fr       */
+/*   Updated: 2021/01/18 22:43:21 by gpaul            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_printf.h"
 
+void	ft_char_flag(t_struct *list, t_flags *flags)
+{
+	if (flags->minus == 0)
+	{
+		while (flags->width > 1)
+		{
+			write(1, " ", 1);
+			flags->width--;
+			list->nbr_print++;
+		}
+	}
+	ft_putchar_fd(list->c_para, 1);
+	if (flags->minus == 1)
+	{
+		while (flags->width > 1)
+		{
+			write(1, " ", 1);
+			flags->width--;
+			list->nbr_print++;
+		}
+	}
+}
+
 void	ft_str_c(char format, t_struct *list, t_flags *flags)
 {
 	int size;
 
-	size = 0;
+	size = ft_strlen(list->s_para);
 	if (format == CONVERT[0] || format == CONVERT[8])
 	{
-		ft_putchar_fd(list->c_para, 1);
-		list->nbr_print = list->nbr_print + 1;
+		if (flags->width > 0)
+			ft_char_flag(list, flags);
+		else
+			ft_putchar_fd(list->c_para, 1);
+		list->nbr_print++;
 	}
 	else
 	{
-		size = ft_strlen(list->s_para);
 		if (flags->width > 0)
 		{
 			size++;
